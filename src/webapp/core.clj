74db12b2e2d1bd6d3c970e20f80db019fa2a1625
vutr <me@vutr.io>
2020-04-12 14:29:09 +0700

    Add config

--- src/webapp/core.clj
+++ src/webapp/core.clj
@@ -1,6 +1,15 @@
 (ns webapp.core  ;(1)
   (:gen-class)
-  (:require [org.httpkit.server :as server])) ;(2)
+  (:require [org.httpkit.server :as server])
+  (:require [aero.core :refer (read-config)])
+  (:require
+    [taoensso.timbre :as timbre
+      :refer [log  trace  debug  info  warn  error  fatal  report
+              logf tracef debugf infof warnf errorf fatalf reportf
+              spy get-env]]))
+
+(def config
+  (read-config "config.edn" {:profile :dev}))
 
 (defn baby-small-app [req] ;(3)
   {:status  200
@@ -10,6 +19,7 @@
 (defn -main ;(4)
   "This is our app's entry point"
   [& args]
-  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))] ;(5)
-    (server/run-server #'baby-small-app {:port port})
-    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
+  (let [server-cfg (:webserver config)]
+    (info server-cfg)
+    (server/run-server #'baby-small-app server-cfg)
+    (println (str "Running webserver at http:/127.0.0.1:" (:port server-cfg) "/"))))
