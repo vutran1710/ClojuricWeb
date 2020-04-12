1183fbcf1837b0ec29eb342feaf889d64bf698fd
vutr <me@vutr.io>
2020-04-12 12:17:31 +0700

    add aero

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [http-kit "2.3.0"]
-                 [metosin/reitit "0.4.2"]]
+                 [metosin/reitit "0.4.2"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [aero "1.1.6"]]
   :main webapp.core
   :repl-options {:init-ns webapp.core}
   :profiles {:user
