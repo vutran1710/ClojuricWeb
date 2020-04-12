9ee2a043d700e448f617a4a03bc8d8045340e64c
vutr <me@vutr.io>
2020-04-12 12:11:14 +0700

    add deps

--- project.clj
+++ project.clj
@@ -3,6 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [http-kit "2.3.0"]
+                 [metosin/reitit "0.4.2"]]
   :main webapp.core
-  :repl-options {:init-ns webapp.core})
+  :repl-options {:init-ns webapp.core}
+  :profiles {:user
+             {
+              :plugins [[lein-kibit "0.1.8"]
+                        [jonase/eastwood "0.3.10"]] }})
--- src/webapp/core.clj
+++ src/webapp/core.clj
@@ -1,8 +1,15 @@
-(ns webapp.core)
+(ns webapp.core  ;(1)
+  (:gen-class)
+  (:require [org.httpkit.server :as server])) ;(2)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn baby-small-app [req] ;(3)
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Pew pew!"})
 
-(foo "Vu")
+(defn -main ;(4)
+  "This is our app's entry point"
+  [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))] ;(5)
+    (server/run-server #'baby-small-app {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
